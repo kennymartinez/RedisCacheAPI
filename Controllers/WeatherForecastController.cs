741eb83a36cf726f3fa70fcb4ea09bf0b11120db
Kenny Martinez <kenny@Kennys-MacBook-Pro.local>
2024-02-02 13:06:42 -0500

    added cache aside pattern to encapsulate data store and caching

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using RedisCacheApi.DataStore;
 
 namespace RedisCacheApi.Controllers;
 
@@ -8,52 +9,20 @@ namespace RedisCacheApi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private const string CACHE_KEY = "WeatherForecast";
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<WeatherForecastController> _logger;
-    private readonly IDistributedCache _cache;
+    private readonly DataRepository _repository;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger,
-        IDistributedCache cache)
+        DataRepository repository)
     {
         _logger = logger;
-        _cache = cache;
+        _repository = repository;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public async Task<IActionResult> Get()
     {
-        // check if data is in cache
-        var cachedData = await _cache.GetStringAsync(CACHE_KEY);
-        if (cachedData != null)
-        {
-            _logger.LogInformation("Obtained from cache");
-            var result = JsonSerializer.Deserialize<WeatherForecast[]>(cachedData);
-            return Ok(result);
-        }
-
-        var cast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-
-        // Cache data for 5 minutes
-        var options = new DistributedCacheEntryOptions
-        {
-            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(3)
-        };
-
-        // Store data in cache
-        var serializedData = JsonSerializer.Serialize(cast);
-        await _cache.SetStringAsync(CACHE_KEY, serializedData, options);
-
-        return Ok(cast);
+        var data = await _repository.GetDataAsync<WeatherForecast[]>(1);
+        return Ok(data);
     }
 }
---
+++ DataStore/DataStore.cs
@@ -0,0 +1,22 @@
+namespace RedisCacheApi.DataStore;
+
+public class DataStore : IDataStore
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    
+    public Task<WeatherForecast[]> GetDataAsync()
+    {
+        var cast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+        return Task.FromResult(cast);
+    }
+}
